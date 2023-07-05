3734c42d5fb4df94e06bf9f455f4c4ef2dd18e5c
codewithme224 <codewithme224@gmail.com>
2023-07-05 16:11:01 -0700

    Fabric script that generates

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,12 +8,11 @@ from datetime import datetime as dt
 
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static"""
-    time = dt.now()
-
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
-    create = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create is not None:
-        return archive
+    local("sudo mkdir -p versions")
+    date = dt.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(date)
+    results = local("sudo tar -cvzf {} web_static".format(file_name))
+    if results.succeeded:
+        return file_name
     else:
         return None
