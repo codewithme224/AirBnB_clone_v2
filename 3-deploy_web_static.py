97c1a4557da75432a0fdadb8562d474f30b9f9a3
codewithme224 <codewithme224@gmail.com>
2023-07-09 10:52:39 -0700

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Script that creates and distributes an archive to your web servers"""
 
-
-from os.path import exists
-from datetime import datetime
+import os.path
+import time
 from fabric.api import local
 from fabric.operations import env, put, run
 
@@ -12,20 +11,21 @@ env.hosts = ['54.162.90.117', '34.229.49.93']
 
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static"""
-    local("sudo mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_{}.tgz".format(date)
-    results = local("sudo tar -cvzf {} web_static".format(file_name))
-    if results.succeeded:
-        return file_name
-    else:
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(time.
+                                                    strftime("%Y%m%d%H%M%S")))
+    except Exception:
         return None
 
 
 def do_deploy(archive_path):
     """Distributes an archive to your web servers"""
-    if exists(archive_path) is False:
+    if (os.path.isfile(archive_path) is False):
         return False
+
     try:
         file_name = archive_path.split("/")[-1]
         new_folder = ("/data/web_static/releases/" + file_name.split(".")[0])
